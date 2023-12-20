67ad425972d1effa49e413083f5ac6fe3b481448
alexzhang1030 <1642114555@qq.com>
2023-12-20 17:00:23 +0800

    chore: migrate to eslint-stylistic

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  target: ['es2015'],
-  format: ['esm'],
+  target: ['es2017'],
+  format: ['esm', 'cjs'],
   clean: true,
   dts: true,
 })
